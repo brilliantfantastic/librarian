212a4e6330d8df7cccd98383babbfcb213c470d8
Keith Thompson <keith@brilliantfantastic.com>
2013-06-17 16:59:45 -0400

    Changed the create books migrate to be an integer, hopefully no one looks in the history to see that I did this

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(:version => 20130617204744) do
 
   create_table "books", :force => true do |t|
     t.string   "name"
-    t.string   "isbn"
+    t.integer  "isbn"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
