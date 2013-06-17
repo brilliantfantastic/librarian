1e612a005117bcfff087125b2e9df6b7999e0c5f
Keith Thompson <keith@brilliantfantastic.com>
2013-06-17 11:42:54 -0400

    Initial commit of librarian (code sample) app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Librarian::Application.config.secret_token = '269af8d55092f27dda894e86ea7ed585703e09853e7dfbbee10289ceb5e0390b7181ddb59ad308bcdbc7c2d059a607d4a9c0d5c47c9157ed569f33cedaa825d0'
