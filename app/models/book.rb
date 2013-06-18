09ed2db0142efbef77b00b88c642338479844d1b
Keith Thompson <keith@brilliantfantastic.com>
2013-06-17 23:33:49 -0400

    Implemented the format validation for Book#isbn

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ActiveRecord::Base
   attr_accessible :isbn, :name, :name_confirmation
 
   validates :name, confirmation: true, presence: true, uniqueness: true
+  validates :isbn, format: { with: /^([0-9]{10}|[0-9]{13})$/ },
+                   allow_nil: true
 end
