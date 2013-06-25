f69dd5426da0cb0a23bfacca0f968b2f3a814843
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 06:48:11 -0400

    Implemented sign in to the point of setting up sorcery

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = login(params[:user][:email], params[:user][:password])
+    redirect_to root_path
   end
 end
