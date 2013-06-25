872e12c90146944f44f3924eb818f2a7ed84586a
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 06:20:43 -0400

    Created the /signin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Librarian::Application.routes.draw do
   resources :books, only: [:new, :create, :show]
   resources :users, only: [:new, :create]
+
+  match '/signin' => 'sessions#new'
+
   root :to => 'reading_materials#index'
 end
