10bd3b7d573ed60630037ecaba2e1ae24c86960c
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 06:39:37 -0400

    Further set up the sessions routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Librarian::Application.routes.draw do
   resources :books, only: [:new, :create, :show]
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:create]
 
   match '/signin' => 'sessions#new'
 
