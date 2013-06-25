7759e10b3d8d670f7fe5bdb120f267f6c800eb51
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 07:43:44 -0400

    Added invalid sign in test

--- test/features/sign_in_test.rb
+++ test/features/sign_in_test.rb
@@ -12,5 +12,14 @@ feature 'Sign In Feature Test' do
     page.must_have_content "Welcome #{user.username}"
   end
 
-  scenario 'signing in with incorrect email or password'
+  scenario 'signing in with incorrect email or password' do
+    user = FactoryGirl.create :user, password: 'testing123'
+    visit signin_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'p@ssword'
+    click_button 'Sign In'
+
+    current_path.must_equal '/sessions'
+    page.must_have_content "Invalid email or password"
+  end
 end
