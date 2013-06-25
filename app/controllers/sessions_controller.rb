cda76cf72a81e997cb472b2f961c43fd4477651d
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 07:49:22 -0400

    Added handling for failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = login(params[:user][:email], params[:user][:password])
+    unless @user
+      @user = User.new
+      flash[:error] = 'Invalid email or password'
+      render :new and return
+    end
     redirect_to root_path, notice: "Welcome #{@user.username}"
   end
 end
