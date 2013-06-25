3936f2e7003258b29b5862d0a3493d5ae045dc9b
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 07:20:52 -0400

    Set up user model to use sorcery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
   attr_accessible :email, :password, :password_confirmation, :username
 
   validates :email, uniqueness: true,
     format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
-  validates :password, presence: true, confirmation: true
+  validates :password, presence: true, confirmation: true, if: :password
   validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130625001758) do
+ActiveRecord::Schema.define(:version => 20130625110128) do
 
   create_table "book_shelves", :force => true do |t|
     t.string   "name"
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(:version => 20130625001758) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.string   "crypted_password"
+    t.string   "salt"
   end
 
 end
