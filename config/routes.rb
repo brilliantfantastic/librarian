04386bce0e99cd9d67f818b6bc032e4bf35dd303
Keith Thompson <keith@brilliantfantastic.com>
2013-07-08 23:59:07 -0400

    Nested the book routes inside of the bookshelves

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Librarian::Application.routes.draw do
-  resources :books, only: [:new, :create, :show]
   resources :users, only: [:new, :create]
   resources :sessions, only: [:create]
 
   match '/signin' => 'sessions#new'
   match '/signout' => 'sessions#destroy'
 
-  resources :shelves, only: [:new, :create, :show]
+  resources :shelves, only: [:new, :create, :show] do
+    resources :books, only: [:new, :create, :show]
+  end
 
   root :to => 'reading_materials#index'
 end
