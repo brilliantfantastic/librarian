7e090a4e7d5b261fb11b430573ebb07d2a808166
Keith Thompson <keith@brilliantfantastic.com>
2013-07-09 05:04:05 -0400

    Wrote test for creating shelf via ujs

--- test/features/creating_a_book_shelf_test.rb
+++ test/features/creating_a_book_shelf_test.rb
@@ -24,4 +24,29 @@ feature 'Creating A BookShelf Feature Test' do
     current_path.must_equal '/shelves'
     page.must_have_content "Name can't be blank"
   end
+
+  describe 'while staying on the same page' do
+    self.use_transactional_fixtures = false
+
+    before do
+      visit library_path
+    end
+
+    after(:each) do
+      User.destroy_all
+      BookShelf.destroy_all
+    end
+
+    scenario 'creating a shelf with valid data', js: true do
+      click_link 'Add bookshelf'
+      within '#new_book_shelf' do
+        fill_in 'Name', with: 'Fantasy'
+        page.find('#book_shelf_name').native.send_keys :return
+      end
+      sleep 1
+      new_shelf = BookShelf.last
+      current_path.must_equal library_path
+      page.must_have_content new_shelf.name
+    end
+  end
 end
