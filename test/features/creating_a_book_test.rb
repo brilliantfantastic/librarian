4f359265bf1efd17b064881faee22e6899c06e5b
Keith Thompson <keith@brilliantfantastic.com>
2013-07-08 23:58:01 -0400

    Adjusted the book creation feature to match how we want the system to work

--- test/features/creating_a_book_test.rb
+++ test/features/creating_a_book_test.rb
@@ -3,11 +3,14 @@ require "test_helper"
 feature "Creating A Book Feature Test" do
   before :each do
     Book.delete_all
+    @user = FactoryGirl.create :user, password: 'password'
+    @shelf = FactoryGirl.create :book_shelf, user: @user
+    signin @user, 'password'
   end
 
   describe "with valid data" do
     before do
-      visit new_book_path
+      visit new_shelf_book_path(@shelf)
       within('#new_book') do
         fill_in 'Book Name', with: 'Treasure Island'
         fill_in 'ISBN', with: '9780670846856'
@@ -20,7 +23,7 @@ feature "Creating A Book Feature Test" do
     end
 
     scenario "redirects to the book's show view" do
-      current_path.must_equal book_path(Book.last)
+      current_path.must_equal shelf_book_path(@shelf, Book.last)
     end
 
     scenario "show view must contain the book's name and isbn" do
@@ -33,7 +36,7 @@ feature "Creating A Book Feature Test" do
   describe "with invalid data" do
     before do
       Book.create(name: 'Treasure Island', isbn: '9780670846856')
-      visit new_book_path
+      visit new_shelf_book_path(@shelf)
     end
 
     scenario "displays errors when submitting empty form" do
@@ -42,7 +45,7 @@ feature "Creating A Book Feature Test" do
       end
 
       page.must_have_content "Name can't be blank"
-      current_path.must_equal '/books'
+      current_path.must_equal "/shelves/#{@shelf.id}/books"
     end
 
     scenario "displays error when book name is already taken" do
@@ -53,7 +56,7 @@ feature "Creating A Book Feature Test" do
       end
 
       page.must_have_content 'Name has already been taken'
-      current_path.must_equal '/books'
+      current_path.must_equal "/shelves/#{@shelf.id}/books"
     end
   end
 end
