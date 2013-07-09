a0a72a857dc64e86448c45ffb4862bfc79c6dc6e
Keith Thompson <keith@brilliantfantastic.com>
2013-07-08 23:29:46 -0400

    Wrote unit tests around bookshelf-user association

--- test/models/book_shelf_test.rb
+++ test/models/book_shelf_test.rb
@@ -9,4 +9,16 @@ describe BookShelf do
     book_shelf.books << book
     book_shelf.books.must_equal [book]
   end
+
+  it 'can have the user_id assigned' do
+    user = FactoryGirl.create :user
+    book_shelf = BookShelf.new(user_id: user.id)
+    book_shelf.valid?.must_equal true
+  end
+
+  it 'is invalid without a user' do
+    book_shelf = BookShelf.new
+    book_shelf.valid?.must_equal false
+    book_shelf.errors.full_messages.must_include "User can't be blank"
+  end
 end
