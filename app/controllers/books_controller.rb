3dd361c3879bd3f79a7cc133cb2355d55630b70b
Keith Thompson <keith@brilliantfantastic.com>
2013-07-15 21:30:54 -0400

    Finish switching over to using carrierwave

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,23 +17,11 @@ class BooksController < ApplicationController
   def create
     @shelf = BookShelf.find(params[:shelf_id])
     ensure_shelf_visible @shelf
-    cover = params[:book].delete(:cover)
-    cover_url = cover ? upload(cover) : nil
-    @book = @shelf.books.create(params[:book].merge(cover_url: cover_url))
+    @book = @shelf.books.create(params[:book])
     if @book.valid?
       redirect_to shelf_book_path(@shelf, @book)
     else
       render :new
     end
-  end
-
-  private
-
-  def upload(cover)
-    file_path = Rails.root.join('public', 'uploads', cover.original_filename)
-    File.open(file_path, 'wb') do |file|
-      file.write(cover.read)
-    end
-    "/uploads/#{cover.original_filename}"
   end
 end
--- test/features/book_cover_upload_test.rb
+++ test/features/book_cover_upload_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 describe 'Book Cover Upload Feature Test' do
   let(:user) { FactoryGirl.create :user, password: 'password' }
   let(:shelf) { FactoryGirl.create :book_shelf, user: user }
-  let(:upload_path) { Rails.root.join('public', 'uploads', 'inferno.jpg') }
+  let(:upload_path) { Rails.root.join('public', 'uploads', 'book') }
 
   before { signin user, 'password' }
-  after { FileUtils.rm(upload_path) }
+  after { FileUtils.rm_rf(upload_path) }
 
   scenario 'uploading a book cover when creating a book' do
     visit new_shelf_book_path(shelf)
@@ -18,6 +18,6 @@ describe 'Book Cover Upload Feature Test' do
     end
 
     current_path.must_equal shelf_book_path(shelf, Book.last)
-    page.must_have_xpath "//img[@src='/uploads/inferno.jpg']"
+    page.must_have_xpath "//img[@src='#{Book.last.cover.url}']"
   end
 end
